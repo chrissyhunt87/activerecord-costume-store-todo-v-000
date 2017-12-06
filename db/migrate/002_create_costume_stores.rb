7028b4d91c027bbdc83bbdafae8363636cc75980
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-06 00:59:27 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,10 +3,16 @@
 class CreateCostumeStores < ActiveRecord::Migration
 
   def change
+    create_table :costume_stores do |t|
+      
+    end
     add_column(:costume_store, :name, :string)
     add_column(:costume_store, :location, :string)
     add_column(:costume_store, :costume_inventory, :string)
-    add_column(:costume_store, :size, :string)
+    add_column(:costume_store, :employee_count, :integer)
+    add_column(:costume_store, :employee_count, :integer)
+    add_column(:costume_store, :employee_count, :integer)
+
   end
 
 end
