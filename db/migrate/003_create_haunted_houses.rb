00985e2cecadf6caa999db79418eecc49e1f8b17
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-06 01:01:49 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
 # Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end
