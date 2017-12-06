6a905fcf58eed6d3a28588f708d5b9fda9cc4e1d
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-06 01:01:32 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,7 +9,6 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration
-
   def change
     create_table :costumes do |t|
       t.string :name
@@ -19,5 +18,4 @@ class CreateCostumes < ActiveRecord::Migration
       t.timestamps
     end
   end
-
 end
