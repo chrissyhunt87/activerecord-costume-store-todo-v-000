c4536f84aa757d18d708f38a9e23ac605999efc8
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-06 00:55:46 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration
+
+  def change
+    add_column(:costumes, :name, :string)
+    add_column(:costumes, :price, :float)
+    add_column(:costumes, :image_url, :string)
+    add_column(:costumes, :size, :string)
+  end
+  
+end
